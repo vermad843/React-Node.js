4086cb94afa9b577585a042bc2d5a9cfe39efe29
vermad843 <vishnuverma7524@gmail.com>
2021-02-14 08:26:55 +0530

    notes in db

--- server/api/notes.js
+++ server/api/notes.js
@@ -1,9 +1,30 @@
 const express = require('express');
 
+const db = require('../db/connection');
+const notes = db.get('notes');
+
 const router = express.Router();
 
 router.get('/', (req,res) => {
    res.json([]);
 });
+
+router.post('/', (req,res) => {
+   const result = req.body;
+   if(result) {
+      const note = {
+         ...req.body,
+         user_id : req.user._id
+      };
+      notes.insert(req.body).then(note => {
+        res.json(note);
+      });
+   }else {
+      const error = new Error(result);
+      res.status(422);
+      next(error);
+   }
+});
+
 
 module.exports = router;
